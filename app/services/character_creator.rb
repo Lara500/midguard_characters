c2352ead3ff6980b7bc951b96f60c401f35e2644
Lara500 <sonia8835@interia.eu>
2022-09-02 15:17:16 +0200

    move logic from controller

---
+++ app/services/character_creator.rb
@@ -0,0 +1,25 @@
+class CharacterCreator < ApplicationService
+
+  def initialize(character_params, parent_name)
+    @character_params = character_params
+    @parent_name = parent_name
+  end
+
+  def call
+    @character = Character.new(@character_params)
+    create_surname
+    return @character.update(@character_params)
+  end
+
+  private
+
+  def create_surname
+    if @character_params["gender"] == "Kobieta"
+      ending = "dottir"
+    elsif @character_params["gender"] == "Mężczyzna"
+      ending = "son"
+    end
+
+    @character.surname = "#{@parent_name}#{ending}"
+  end
+end
