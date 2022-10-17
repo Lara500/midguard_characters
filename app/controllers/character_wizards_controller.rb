6c9152b0c7f0a0ad9347723c9fd07bc2fa2b1d47
Lara500 <sonia8835@interia.eu>
2022-10-17 13:45:05 +0200

    seperate tasks from service object to create

--- app/controllers/character_wizards_controller.rb
+++ app/controllers/character_wizards_controller.rb
@@ -6,12 +6,13 @@ class CharacterWizardsController < ApplicationController
       @kind_gender += Kind.where(gender: "Dowolna")
       render "kind_and_benefits", local: @kind_gender
     elsif params["step"] == "benefits"
-      kind_and_benefits
+      throw(params)
+      show_benefits
     end
   end
 
   def create
-    result = CharacterWizardService.call(wizard_params, params["char_id"], params["step"])
+    result = self.send(params["step"].to_sym)
     if result
       if params["step"] == "kind_and_benefits"
        redirect_to(character_wizard_path(char_id: params["char_id"], step: "benefits"))
@@ -24,18 +25,38 @@ class CharacterWizardsController < ApplicationController
     end
   end
 
-  def kind_and_benefits
+  def show_benefits
     kind_name = Kind.find(params["char_id"]).name
     @benefits = Benefit.where(kind_name: kind_name)
     render "benefits", :object => @benefits
   end
 
-  def wizard_params
-    if params["step"] == "kind_and_benefits"
-      params.permit(:kind_char)
-    elsif params["step"] == "benefits"
-      params.permit(:benefit_first, :name_one, :benefit_second, :name_two, :benefit_third, :name_three)
-    end
+  private
+
+  def kind_and_benefits
+    @character_wizard = CharacterWizard.new(kind_char: params["kind_char"], character_id: @char_id)
+    @character_wizard.save
+    @character_wizard
   end
+
+  def benefits
+    @character_wizard = CharacterWizard.find_by_character_id(@char_id)
+    if params["benefit_first"] == "1" && params["benefit_second"] == "1"
+      @character_wizard.update(first_benefit: params["name_one"], second_benefit: params["name_two"])
+    elsif params["benefit_second"] == "1" && params["benefit_third"] == "1"
+      @character_wizard.update(first_benefit: params["name_two"], second_benefit: params["name_three"])
+    elsif params["benefit_first"] == "1" && params["benefit_third"] == "1"
+      @character_wizard.update(first_benefit: params["name_one"], second_benefit: params["name_three"])
+    end
+    @character_wizard
+  end
+
+  # def wizard_params
+  #   if params["step"] == "kind_and_benefits"
+  #     params.permit(:kind_char)
+  #   elsif params["step"] == "benefits"
+  #     params.permit(:benefit_first, :name_one, :benefit_second, :name_two, :benefit_third, :name_three)
+  #   end
+  # end
 
 end
