e814e364c3f98d6cb4cfc1e44cdd7b1271036696
Lara500 <sonia8835@interia.eu>
2022-10-18 13:14:03 +0200

    change adding of chosen benefits

--- app/controllers/character_wizards_controller.rb
+++ app/controllers/character_wizards_controller.rb
@@ -25,7 +25,7 @@ class CharacterWizardsController < ApplicationController
   end
 
   def show_benefits
-    kind_name = CharacterWizard.find(params["char_id"]).kind_char
+    kind_name = CharacterWizard.find_by_character_id(params["char_id"]).kind_char
     @benefits = Benefit.where(kind_name: kind_name)
     render "benefits", :object => @benefits
   end
@@ -33,29 +33,18 @@ class CharacterWizardsController < ApplicationController
   private
 
   def kind_and_benefits
-    @character_wizard = CharacterWizard.new(kind_char: params["kind_char"], character_id: @char_id)
+    @character_wizard = CharacterWizard.new(kind_char: params["kind_char"], character_id: params["char_id"])
     @character_wizard.save
     @character_wizard
   end
 
   def benefits
-    @character_wizard = CharacterWizard.find_by_character_id(@char_id)
-    if params["benefit_first"] == "1" && params["benefit_second"] == "1"
-      @character_wizard.update(first_benefit: params["name_one"], second_benefit: params["name_two"])
-    elsif params["benefit_second"] == "1" && params["benefit_third"] == "1"
-      @character_wizard.update(first_benefit: params["name_two"], second_benefit: params["name_three"])
-    elsif params["benefit_first"] == "1" && params["benefit_third"] == "1"
-      @character_wizard.update(first_benefit: params["name_one"], second_benefit: params["name_three"])
-    end
+    choices = params.select { |key, value| value.to_s.match("1") && key.to_s.match("benefit_")}
+    choices_name = params.select { |key, value| key.to_s.match(choices.keys[0]) || key.to_s.match(choices.keys[1])}
+
+    @character_wizard = CharacterWizard.find_by_character_id(params["char_id"])
+    @character_wizard.update(first_benefit: choices_name.values[1], second_benefit: choices_name.values[3])
     @character_wizard
   end
-
-  # def wizard_params
-  #   if params["step"] == "kind_and_benefits"
-  #     params.permit(:kind_char)
-  #   elsif params["step"] == "benefits"
-  #     params.permit(:benefit_first, :name_one, :benefit_second, :name_two, :benefit_third, :name_three)
-  #   end
-  # end
-
+  
 end
