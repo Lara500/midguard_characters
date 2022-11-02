6fe0c94357f1ede9e1fb9ed1cf378c5b88ad6a36
Lara500 <sonia8835@interia.eu>
2022-11-02 12:12:23 +0100

    remove unnecessary migrations and model

--- db/migrate/20221003123839_add_kind_to_skills.rb
+++
@@ -1,5 +0,0 @@
-class AddKindToSkills < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :skills, :kinds, foreign_key: true
-  end
-end
--- db/migrate/20221010090229_change_skills_names.rb
+++
@@ -1,8 +0,0 @@
-class ChangeSkillsNames < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :skills, :white_weapon, :side_arm
-    rename_column :skills, :resilience, :endurance
-    rename_column :skills, :grapple, :hand_to_hand_combat
-    rename_column :skills, :technique, :engineering
-  end
-end
