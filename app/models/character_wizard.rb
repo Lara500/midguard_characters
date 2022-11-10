f896ab5b10cc501f5c4464000facda25101ac8ca
Lara500 <sonia8835@interia.eu>
2022-11-10 14:22:01 +0100

    Add relation to parameter

--- app/models/character_wizard.rb
+++ app/models/character_wizard.rb
@@ -1,2 +1,6 @@
 class CharacterWizard < ApplicationRecord
+  has_many :parameters, as: :entity
+  has_many :main_parameters, -> { where(type: "MainParameter") }, as: :entity
+  has_many :skill_parameters, -> { where(type: "SkillParameter") }, as: :entity
+  has_many :auxiliary_parameters, -> { where(type: "AuxiliaryParameter") }, as: :entity
 end
